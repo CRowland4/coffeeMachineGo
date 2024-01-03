8d8513e0be32d69dde7b112e3dbf255711018e32
Caleb Rowland <cjrowland4@gmail.com>
2024-01-03 14:09:01 -0600

    Add third stage of Coffee Machine project

--- Coffee Machine (Go)/task/main.go
+++ Coffee Machine (Go)/task/main.go
@@ -3,16 +3,49 @@ package main
 import "fmt"
 
 func main() {
-	var cupCount int
+	// Define resource constants
+	mlWaterPerCup := 200
+	mlMilkPerCup := 50
+	gramsCoffeePerCup := 15
+
+	// Gather the amount of resources the coffee machine has
+	var waterSupply int
+	var milkSupply int
+	var coffeeGramSupply int
+
+	fmt.Println("Write how many ml of water the coffee machine has:")
+	fmt.Scan(&waterSupply)
+
+	fmt.Println("Write how many ml of milk the coffee machine has:")
+	fmt.Scan(&milkSupply)
+
+	fmt.Println("Write how many grams of coffee beans the coffee machine has:")
+	fmt.Scan(&coffeeGramSupply)
+
+	/* Figure out how many cups of coffee the machine can make.
+	This will be determined by the resource that can make the fewest cups.
+	*/
+	cupsSupply := waterSupply / mlWaterPerCup
+
+	if milkSupply/mlMilkPerCup < cupsSupply {
+		cupsSupply = milkSupply / mlMilkPerCup
+	}
+
+	if coffeeGramSupply/gramsCoffeePerCup < cupsSupply {
+		cupsSupply = coffeeGramSupply / gramsCoffeePerCup
+	}
+
+	// Get the requested amount of cups of coffee
+	var cupsOrdered int
 	fmt.Println("Write how many cups of coffee you will need:")
-	fmt.Scan(&cupCount)
+	fmt.Scan(&cupsOrdered)
 
-	waterNeeded := cupCount * 200
-	milkNeeded := cupCount * 50
-	coffeeNeeded := cupCount * 15
-
-	fmt.Printf("For %d cups of coffee you will need:\n", cupCount)
-	fmt.Printf("%d ml of water\n", waterNeeded)
-	fmt.Printf("%d ml of milk\n", milkNeeded)
-	fmt.Printf("%d g of coffee beans\n", coffeeNeeded)
+	// Respond to the user
+	if cupsOrdered < cupsSupply {
+		fmt.Printf("Yes, I can make that amount of coffee (and even %d more than that)\n", cupsSupply-cupsOrdered)
+	} else if cupsOrdered == cupsSupply {
+		fmt.Println("Yes, I can make that amount of coffee")
+	} else {
+		fmt.Printf("No, I can make only %d cups of coffee\n", cupsSupply)
+	}
 }
